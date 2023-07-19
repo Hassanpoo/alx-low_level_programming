f55061d8f86fd46b8315e7410d60f3c4ab35f0cb
Hassanpoo <poalipooo@gmail.com>
2023-07-19 13:07:51 -0700

    els if while

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,31 +1,21 @@
 #include "main.h"
 
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-
-#endif /* MAIN_H */
-
-
 /**
  * main - Entry point
  *
- * Description: Prints _putchar  using putchar prototype
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
-}
+
