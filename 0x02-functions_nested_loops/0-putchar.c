8ab521099c206ce2ba8b5931deb9cbd296a39d30
Hassanpoo <poalipooo@gmail.com>
2023-07-18 17:50:06 -0700

    c loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.h" 
+
+    // Assuming there's a header file called "_putchar.h" containing the declaration of the _putchar function.
+
+   int main(void) {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return 0;
+}
