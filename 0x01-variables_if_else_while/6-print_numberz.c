9a722298eff417c8b0dba39f98df97b093b8a518
Hassanpoo <poalipooo@gmail.com>
2023-07-18 17:26:41 -0700

    if while els

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++)
+        putchar(digit % 10 + '0');
+
+    putchar('\n');
+
+    return 0;
+}
