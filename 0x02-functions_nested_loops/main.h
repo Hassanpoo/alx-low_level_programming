f28063f3a49a9196c1f532513c6d9fa393d94118
Hassanpoo <poalipooo@gmail.com>
2023-07-19 19:19:41 -0700

    0*02 C

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-void print_alphabet(void);
 int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
-#endif 
+
+#endif /* MAIN_H */
