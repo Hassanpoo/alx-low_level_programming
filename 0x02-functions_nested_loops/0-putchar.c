1c0ee440052a752b60bb2c764db0255aad18512a
Hassanpoo <poalipooo@gmail.com>
2023-07-19 17:52:01 -0700

    0*02 C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,16 @@
 #include "main.h"
 
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-
-#endif /* MAIN_H */
-
-
-
-
-/**
- * maim.h - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	return (0);
+    return 0;
 }
