c5c038b8d436309b298847d7f24a57bd7c3fed07
Hassanpoo <poalipooo@gmail.com>
2023-07-19 20:03:17 -0700

    0*02 C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,5 +7,30 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = 
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 
+		for (j = 1; j <= 9; j++)
+		{
+			result = i * j;
+
+			if (result < 10)
+				_putchar(' ');
+			else
+				_putchar('0' + result / 10);
+
+			_putchar('0' + result % 10);
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
